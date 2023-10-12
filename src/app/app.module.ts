ddcd769f9117c69358d7dc6c0f7f2732bbe1fcc2
Wojciech Krzysztofowicz <wojciech.krzysztofowicz@gmail.com>
2023-10-12 21:44:53 +0200

    Add api clients module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiClientsModule } from '@api-clients';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ApiClientsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
