5e260349695acff62dc2e201c7c40a220a0b011e
Wojciech Krzysztofowicz <wojciech.krzysztofowicz@gmail.com>
2024-03-02 16:09:34 +0100

    Refactor - replace async pipe with signals, add optimalizations for component rendering (set changeDetectionStrategy.onPush)

--- src/app/ui/cards/base/components/two-players-cards-layout/two-players-cards-layout.component.ts
+++ src/app/ui/cards/base/components/two-players-cards-layout/two-players-cards-layout.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, Signal, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { TranslateModule } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
 import { CardsLayoutComponent } from '../cards-layout/cards-layout.component';
 import { CardService } from '../../services/card.service';
 import { CardPlayer } from '../../models/card-player';
@@ -10,18 +9,19 @@ import { CardPlayerComponent } from '../card-player/card-player.component';
 @Component({
   selector: 'sw-two-players-cards-layout',
   standalone: true,
-  imports: [AsyncPipe, TranslateModule, CardsLayoutComponent, CardPlayerComponent],
+  imports: [TranslateModule, CardsLayoutComponent, CardPlayerComponent],
   templateUrl: './two-players-cards-layout.component.html',
-  styleUrls: ['./two-players-cards-layout.component.scss']
+  styleUrls: ['./two-players-cards-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TwoPlayersCardsLayoutComponent {
   @Input() title: string;
 
   private readonly cardService = inject(CardService);
 
-  player1$: Observable<CardPlayer> = this.cardService.player1$;
-  player2$: Observable<CardPlayer> = this.cardService.player2$;
-  isLoading$: Observable<boolean> = this.cardService.isLoading$;
+  player1: Signal<CardPlayer> = toSignal(this.cardService.player1$);
+  player2: Signal<CardPlayer> = toSignal(this.cardService.player2$);
+  isLoading: Signal<boolean> = toSignal(this.cardService.isLoading$);
 
   draw(): void {
     this.cardService.draw();
